00438d8acca2ba25daf85518a88a8cc4d4d0482a
xlfliu <aemidnight@gmail.com>
2019-01-09 11:45:46 +0800

    增加文章编写bat，并写了个教程

---
+++ 写文章.bat
@@ -0,0 +1,11 @@
+@echo off
+mode con lines=30 cols=60
+cd C:\Users\xsin\Documents\Github\xsin\ 
+echo.================================
+echo.       请输入标题
+echo.================================
+set /p name=标题:
+echo name:%name%
+echo please wait
+hexo new %name% && call 3.bat %name%
+pause
