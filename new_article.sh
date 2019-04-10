b421e6d54da12bfcd6b6ba7a34a4e004b9aec54a
xfliu <aemidnight@gmail.com>
2019-04-10 21:02:53 +0800

    update

--- new_article.sh
+++ new_article.sh
@@ -2,10 +2,10 @@
 echo "请输入文章名"
 read title
 echo "$title is your title"
-cd /home/xsin/xsin
+cd /home/xsin/Documents/x51n
 hexo new $title
 echo "创建成功"
-cd /home/xsin/xsin/source/_posts/
+cd /home/xsin/Documents/x51n/source/_posts/
 titles=$(find . -iname "*$title.md")
 echo $titles
 gnome-open $titles && exit
